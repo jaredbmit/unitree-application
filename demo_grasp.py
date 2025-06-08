8409ac9cd1637f6d5c6078ec92338beb2521d6c2
jared <jaredb@mit.edu>
2025-06-08 16:23:00 -0400

    demo script

--- demo_grasp.py
+++ demo_grasp.py
@@ -5,15 +5,27 @@ from multiprocessing import Array, Lock
 
 from avp_teleoperate.teleop.robot_control.robot_arm import G1_29_ArmController
 from avp_teleoperate.teleop.robot_control.robot_arm_ik import G1_29_ArmIK
-from avp_teleoperate.teleop.robot_control.robot_hand_unitree import Dex3_1_Controller
+from avp_teleoperate.teleop.robot_control.robot_hand_unitree import Dex3_1_Simple_Controller
 
 if __name__ == "__main__":
     print("Instantiate controllers")
     arm_ctrl = G1_29_ArmController()
     arm_ik = G1_29_ArmIK()
+	
+    print("Wait for warmup")
+    time.sleep(2.5)
 
-    print("Return to home pose")
-    arm_ctrl.ctrl_dual_arm_go_home()
+    print("Commanding grippers")
+    hand_ctrl = Dex3_1_Simple_Controller()
+    print("Command 0")
+    hand_ctrl.ctrl_dual_hand_binary(False, False)
+    time.sleep(2)
+    print("Command 1")
+    hand_ctrl.ctrl_dual_hand_binary(True, True)
+    time.sleep(5)
+    print("Command 2")
+    hand_ctrl.ctrl_dual_hand_binary(False, False)
+    time.sleep(2)
 
     # initial positon
     T_left_init = pin.SE3(
@@ -25,15 +37,40 @@ if __name__ == "__main__":
         np.array([0.25, -0.25, 0.1]),
     )
 
-    dt = 0.05
-    duration = 5
+    dt = 0.01
+    duration = 10
     n = int(duration / dt)
-    delta_translation = np.array([0.1, 0, 0])
-    T_left = [pin.SE3(T_left_init.rotation, T_left_init.translation + delta_translation * i / n) for i in range(n)]
-    T_right = [pin.SE3(T_right_init.rotation, T_right_init.translation + delta_translation * i / n) for i in range(n)]
+    delta_translation = np.array([0.15, 0, 0.25])
+    delta_angle = - np.pi / 2
+    T_left = [T_left_init for i in range(n)]
+    T_right = [
+        pin.SE3(
+            pin.Quaternion(np.cos(delta_angle * i / (n // 4) / 2), np.sin(delta_angle * i / (n // 4) / 2), 0, 0),
+            T_right_init.translation + delta_translation * i / (n // 4)
+        ) 
+        for i in range(n // 4)
+    ] + [
+        pin.SE3(
+            pin.Quaternion(np.cos(delta_angle / 2), np.sin(delta_angle / 2), 0, 0),
+            T_right_init.translation + delta_translation - np.array([0, 0, 0.15]) * i / (n // 4)
+        )
+        for i in range(n // 4)
+    ] + [
+        pin.SE3(
+            pin.Quaternion(np.cos(delta_angle / 2), np.sin(delta_angle / 2), 0, 0),
+            T_right_init.translation + delta_translation - np.array([0, 0, 0.15])
+        )
+        for i in range(n // 4)
+    ] + [
+        pin.SE3(
+            pin.Quaternion(np.cos(delta_angle / 2), np.sin(delta_angle / 2), 0, 0),
+            T_right_init.translation + delta_translation - np.array([0, 0, 0.15]) * (1 - i / (n // 4))
+        )
+        for i in range(n // 4)
+    ]
 
     print("Run trajectory")
-    arm_ctrl.speed_gradual_max(t=2.0)  # Avoid jerky start
+    arm_ctrl.speed_gradual_max(t=1.0)  # Avoid jerky start
     for i in range(n):
         start_time = time.time()
 
@@ -46,7 +83,7 @@ if __name__ == "__main__":
         current_lr_arm_dq = arm_ctrl.get_current_dual_arm_dq()
 
         # solve ik using motor data and wrist pose, then use ik results to control arms.
-        sol_q, sol_tauff  = arm_ik.solve_ik(left_wrist, right_wrist, current_lr_arm_q, current_lr_arm_dq)
+        sol_q, sol_tauff  = arm_ik.solve_ik(left_wrist.homogeneous, right_wrist.homogeneous, current_lr_arm_q, current_lr_arm_dq)
         arm_ctrl.ctrl_dual_arm(sol_q, sol_tauff)
 
         current_time = time.time()
@@ -54,13 +91,3 @@ if __name__ == "__main__":
         sleep_time = max(0, dt - time_elapsed)
         time.sleep(sleep_time)
 
-    # # TODO actuate grippers
-    # left_hand_array = Array('d', 75, lock = True)          # [input]
-    # right_hand_array = Array('d', 75, lock = True)         # [input]
-    # dual_hand_data_lock = Lock()
-    # dual_hand_state_array = Array('d', 14, lock = False)   # [output] current left, right hand state(14) data.
-    # dual_hand_action_array = Array('d', 14, lock = False)  # [output] current left, right hand action(14) data.
-    # hand_ctrl = Dex3_1_Controller(left_hand_array, right_hand_array, dual_hand_data_lock, dual_hand_state_array, dual_hand_action_array)
-
-    print("Return to home pose")
-    arm_ctrl.ctrl_dual_arm_go_home()
